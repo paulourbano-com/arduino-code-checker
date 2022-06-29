cfef9cfe53fc6ccb045d32cbdcf53ea55cc68b20
paulourbano <pgurbano@gmail.com>
2022-06-29 02:13:43 -0300

    Base procedure to download Arduino code and circuit definition.

--- arduino_code_checker/src/download_code.py
+++ arduino_code_checker/src/download_code.py
@@ -1,11 +1,23 @@
+import traceback
 from playwright.sync_api import Playwright, sync_playwright, expect
 from typing import List
-import time
+import os
+import shutil
+import datetime
 
 
 def run(
-    playwright: Playwright, student_list: List[str], assigment_list: List[str]
+    playwright: Playwright,
+    student_list: List[str],
+    assigment_list: List[str],
+    download_folder: str = "tinkercad_downloads",
 ) -> None:
+
+    if os.path.isdir(download_folder):
+        shutil.rmtree(download_folder)
+
+    os.mkdir(download_folder)
+
     browser = playwright.firefox.launch(headless=True)
     context = browser.new_context(viewport={"width": 1600, "height": 1200})
 
@@ -60,47 +72,121 @@ def run(
         page.locator('a[role="button"]:has-text("2022.1 Computação 1 - IF71A")').click()
 
     for current_student in student_list:
-        print(f"{current_student=}")
-        page.goto("https://www.tinkercad.com/classrooms/cNhyXcecgXF")
+        print(f"\n{current_student=}")
+        # page.goto("https://www.tinkercad.com/classrooms/cNhyXcecgXF")
 
-        with page.expect_navigation():
-            page.locator(f"text={current_student}").click()
+        # with page.expect_navigation():
+        #     page.locator(f"text={current_student}").click()
 
-        page.locator("#content >> text=Circuits").click()
+        # page.locator("#content >> text=Circuits").click()
 
         for current_assigment in assigment_list:
-            print(f"{current_assigment=}")
-            # Click h3:has-text("E1T1")
-            page.locator(f'h3:has-text("{current_assigment}")').click()
+            retries = 3
+            success = False
 
-            # Click a:has-text("Simulate")
-            page.locator('a:has-text("Simulate")').click()
+            while retries > 0 and not success:
+                print(f"\t{current_assigment=}")
 
-            page.locator('a:has-text("Code")').first.click()
+                source_file = os.path.join(
+                    download_folder,
+                    f'{current_student.replace(" ", "")}_{current_assigment}_code.ino',
+                )
+                pcb_file = os.path.join(
+                    download_folder,
+                    f'{current_student.replace(" ", "")}_{current_assigment}_circuit.brd',
+                )
 
-            with page.expect_download() as download_info:
-                page.frame_locator(
-                    "text=Add an image of this Thing JPG, GIF or PNG image that is under 5MB UploadSimulat >> iframe"
-                ).locator(".circ_btn__icon.cio-ui-icon > svg").first.click()
-            download = download_info.value
+                page.goto("https://www.tinkercad.com/classrooms/cNhyXcecgXF")
 
-            saved_download_file = (
-                f'{current_student.replace(" ", "")}_{current_assigment}.ino'
-            )
-            print(f"Saving {saved_download_file}...\n")
-            download.save_as(saved_download_file)
+                try:
+                    with page.expect_navigation():
+                        page.locator(f"text={current_student}").click()
+
+                    page.locator("#content >> text=Circuits").click()
+
+                    all_results = page.locator(f'h3:has-text("{current_assigment}")')
+                    all_results.nth(0).wait_for()
+
+                    results_count = all_results.count()
+
+                    # print(results_count)
+
+                    base_page = page.url
+
+                    for index in range(results_count):
+                        # page.locator(f'h3:has-text("{current_assigment}")').click()
+                        if results_count > 1:
+                            source_file = os.path.join(
+                                download_folder,
+                                f'{current_student.replace(" ", "")}_{current_assigment}-{str(index+1).zfill(2)}_code.ino',
+                            )
+                            pcb_file = os.path.join(
+                                download_folder,
+                                f'{current_student.replace(" ", "")}_{current_assigment}-{str(index+1).zfill(2)}_circuit.brd',
+                            )
+
+                        all_results.nth(index).click()
+
+                        page.goto(f"{page.url}/editel")
+
+                        page.locator("#CODE_EDITOR_ID >> text=Code").click()
+
+                        # Click .circ_btn.circ_btn--m_icon >> nth=0
+                        with page.expect_download() as download_info:
+                            page.locator(".circ_btn.circ_btn--m_icon").first.click()
+                        download = download_info.value
+                        download.save_as(source_file)
+                        print(f"\t\tSaving {source_file}...")
+
+                        # Click text=Send To
+                        page.locator("text=Send To").click()
+
+                        # Click text=.BRD >> nth=1
+                        with page.expect_download() as download_info:
+                            page.locator("text=.BRD").nth(1).click()
+                        download = download_info.value
+                        download.save_as(pcb_file)
+                        print(f"\t\tSaving {pcb_file}...\n")
+
+                        page.goto(base_page)
+                        page.locator(f'h3:has-text("{current_assigment}")').nth(
+                            0
+                        ).wait_for()
+                except:
+                    print(traceback.format_exc())
+                    retries = retries - 1
+
+                success = True
 
     # ---------------------
     context.close()
     browser.close()
 
 
+start = datetime.datetime.now()
 with sync_playwright() as playwright:
-    run(playwright, ["Michel Martins Pereira", "TroyRogerJr"], ["E1T1"])
-
-    # # Click a:has-text("Code") >> nth=0
-    # page.frame_locator("text=Add an image of this Thing JPG, GIF or PNG image that is under 5MB UploadSimulat >> iframe").locator("a:has-text(\"Code\")").first.click()
-    # # Click .circ_btn.circ_btn--m_icon >> nth=0
-    # with page.expect_download() as download_info:
-    #     page.frame_locator("text=Add an image of this Thing JPG, GIF or PNG image that is under 5MB UploadSimulat >> iframe").locator(".circ_btn.circ_btn--m_icon").first.click()
-    # download = download_info.value
+    run(
+        playwright,
+        [
+            # "Michel Martins Pereira",
+            # "TroyRogerJr",
+            # "tarekhenrique",
+            # "MURILO JOSE IGNACIO",
+            # "noamserique",
+            # "Luiz Miguel Nascimento",
+            # "Murilo Ignácio",
+            # "JuliaValderes",
+            # "viniciusrolim",
+            # "thaispercebon",
+            # "VINICIUS FERREIRA PINTO DE OLIVEIRA",
+            # "Paulo Kupczak",
+            # "Rafael Ferraz de Campos",
+            # "luiza.2022",
+            # "WILLIAM AMORIM DA COSTA",
+            # "RAFAEL MURILO CHAMPINI",
+            "JUAN VITOR MACANHAO CASIMIRO",
+        ],
+        ["E1T1"],  # , "E1T2", "E2T1", "E2T2", "E3T1"],
+    )
+end = datetime.datetime.now()
+print(f"Time elapsed: {end - start}s")
