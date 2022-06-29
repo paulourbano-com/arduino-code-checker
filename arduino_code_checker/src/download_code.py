5742a8330fb9bce8782362b4341164cae031d1f7
paulourbano <pgurbano@gmail.com>
2022-06-29 11:45:15 -0300

    Removing dead code. Solving bug of download retries. Reducing timeout for 15s. Adding code to collect student list.

--- arduino_code_checker/src/download_code.py
+++ arduino_code_checker/src/download_code.py
@@ -1,6 +1,6 @@
 import traceback
 from playwright.sync_api import Playwright, sync_playwright, expect
-from typing import List
+from typing import List, Optional
 import os
 import shutil
 import datetime
@@ -8,8 +8,8 @@ import datetime
 
 def run(
     playwright: Playwright,
-    student_list: List[str],
     assigment_list: List[str],
+    student_list: Optional[List[str]] = None,
     download_folder: str = "tinkercad_downloads",
 ) -> None:
 
@@ -25,60 +25,52 @@ def run(
     page = context.new_page()
 
     print("Opening TinkerCAD...")
-    # Go to https://www.tinkercad.com/
     page.goto("https://www.tinkercad.com/")
 
-    # Click text=Log In
-    # with page.expect_navigation(url="https://www.tinkercad.com/login"):
     print("Opening LogIn...")
     with page.expect_navigation():
         page.locator("text=Log In").click()
 
-    # Click text=Personal accounts
     print("Opening Personal Accounts...")
     page.locator("text=Personal accounts").click()
-
-    # Click a:has-text("Email or Username")
 
     print("Opening Enter Email...")
     with page.expect_navigation():
         page.locator('a:has-text("Email or Username")').click()
 
-    # Fill input[name="UserName"]
     page.locator('input[name="UserName"]').fill("paulo@paulourbano.com")
-
-    # Click [aria-label="Next button"]
 
     print("Opening Password...")
     with page.expect_navigation():
         page.locator('[aria-label="Next button"]').click()
 
-    # Click [aria-label="Password text field"]
     page.locator('[aria-label="Password text field"]').click()
 
-    # Fill [aria-label="Password text field"]
     page.locator('[aria-label="Password text field"]').fill("TinkerCAD2022")
 
-    # Click [aria-label="Sign in button"]
-    # with page.expect_navigation(url="https://www.tinkercad.com/dashboard"):
     print("Opening Submit...")
     with page.expect_navigation():
         page.locator('[aria-label="Sign in button"]').click()
 
-    # Click a[role="button"]:has-text("2022.1 Computação 1 - IF71A")
-    # with page.expect_navigation(url="https://www.tinkercad.com/classrooms/cNhyXcecgXF"):
     print("Opening Classroom...")
     with page.expect_navigation():
         page.locator('a[role="button"]:has-text("2022.1 Computação 1 - IF71A")').click()
 
+    if student_list is None:
+        locator = page.locator("//span[contains(@class, 'name ng-star-inserted')]")
+        locator.nth(0).wait_for()
+
+        count_rows = locator.count()
+
+        student_list = []
+        for index in range(count_rows):
+            student_name = locator.nth(index).inner_text()
+            student_list.append(student_name)
+
+    print(f"{student_list=}")
+
     for current_student in student_list:
         print(f"\n{current_student=}")
-        # page.goto("https://www.tinkercad.com/classrooms/cNhyXcecgXF")
-
-        # with page.expect_navigation():
-        #     page.locator(f"text={current_student}").click()
-
-        # page.locator("#content >> text=Circuits").click()
 
         for current_assigment in assigment_list:
             retries = 3
@@ -105,16 +97,13 @@ def run(
                     page.locator("#content >> text=Circuits").click()
 
                     all_results = page.locator(f'h3:has-text("{current_assigment}")')
-                    all_results.nth(0).wait_for()
+                    all_results.nth(0).wait_for(timeout=15000)
 
                     results_count = all_results.count()
-
-                    # print(results_count)
 
                     base_page = page.url
 
                     for index in range(results_count):
-                        # page.locator(f'h3:has-text("{current_assigment}")').click()
                         if results_count > 1:
                             source_file = os.path.join(
                                 download_folder,
@@ -131,17 +120,14 @@ def run(
 
                         page.locator("#CODE_EDITOR_ID >> text=Code").click()
 
-                        # Click .circ_btn.circ_btn--m_icon >> nth=0
                         with page.expect_download() as download_info:
                             page.locator(".circ_btn.circ_btn--m_icon").first.click()
                         download = download_info.value
                         download.save_as(source_file)
                         print(f"\t\tSaving {source_file}...")
 
-                        # Click text=Send To
                         page.locator("text=Send To").click()
 
-                        # Click text=.BRD >> nth=1
                         with page.expect_download() as download_info:
                             page.locator("text=.BRD").nth(1).click()
                         download = download_info.value
@@ -152,46 +138,22 @@ def run(
                         page.locator(f'h3:has-text("{current_assigment}")').nth(
                             0
                         ).wait_for()
+                        success = True
                 except:
                     print(traceback.format_exc())
                     retries = retries - 1
 
-                success = True
-
-    # ---------------------
     context.close()
     browser.close()
 
 
-start = datetime.datetime.now()
-with sync_playwright() as playwright:
-    run(
-        playwright,
-        [
-            "Michel Martins Pereira",
-            "TroyRogerJr",
-            "tarekhenrique",
-            "MURILO JOSE IGNACIO",
-            "noamserique",
-            "Luiz Miguel Nascimento",
-            "Murilo Ignácio",
-            "JuliaValderes",
-            "viniciusrolim",
-            "thaispercebon",
-            "VINICIUS FERREIRA PINTO DE OLIVEIRA",
-            "Paulo Kupczak",
-            "Rafael Ferraz de Campos",
-            "luiza.2022",
-            "WILLIAM AMORIM DA COSTA",
-            "RAFAEL MURILO CHAMPINI",
-            "JUAN VITOR MACANHAO CASIMIRO",
-            "Paulo Wilson Biss",
-            "KLEITON HENRIQUE DE LIMA SOUZA",
-            "HenriqueCalhariSenai",
-            "Vinicius Marcon Rodrigues da Silva",
-            "Reginaldo Camargo Pires",
-        ],
-        ["E1T1", "E1T2", "E2T1", "E2T2", "E3T1"],
-    )
-end = datetime.datetime.now()
-print(f"Time elapsed: {end - start}s")
+if __name__ == "__main__":
+    start = datetime.datetime.now()
+    with sync_playwright() as playwright:
+        run(
+            playwright,
+            student_list=["luiza.2022"],
+            assigment_list=["E1T1"],  # , "E1T2", "E2T1", "E2T2", "E3T1"],
+        )
+    end = datetime.datetime.now()
+    print(f"Time elapsed: {end - start}s")
