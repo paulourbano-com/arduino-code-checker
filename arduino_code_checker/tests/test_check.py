9f1117d3c9f9ea37cf11bef900e53d1e8c67ab2a
paulourbano <pgurbano@gmail.com>
2022-06-30 00:43:46 -0300

    Refactoring tests.

--- arduino_code_checker/tests/test_check.py
+++ arduino_code_checker/tests/test_check.py
@@ -1,48 +1,50 @@
 import os
+import pandas as pd
 from arduino_code_checker.src.code_check import (
-    compare_solution_assigment,
+    compare_solution_assignment,
     compare_set_inner_func_calls,
+    batch_compare,
 )
 
 
 def test_check_01():
-    percent_match = compare_solution_assigment(
+    result = compare_solution_assignment(
         os.path.join(os.path.dirname(__file__), "solution_01", "solution.ino"),
         os.path.join(os.path.dirname(__file__), "assigments_01", "assigment.ino"),
     )
 
-    assert percent_match == 100.0
+    assert sum(result.values()) / len(result) == 100.0
 
 
 def test_non_existant_file():
-    percent_match = compare_solution_assigment(
+    result = compare_solution_assignment(
         os.path.join(os.path.dirname(__file__), "solution_01", "solution.ino"),
         os.path.join(os.path.dirname(__file__), "assigments_01", "not_there.ino"),
     )
 
-    assert percent_match == -1.0
+    assert sum(result.values()) / len(result) == 0.0
 
 
 def test_parse_error():
-    percent_match = compare_solution_assigment(
+    result = compare_solution_assignment(
         os.path.join(os.path.dirname(__file__), "solution_01", "solution.ino"),
         os.path.join(
             os.path.dirname(__file__), "assigments_01", "assigment_syntax_error.ino"
         ),
     )
 
-    assert percent_match == -1.0
+    assert sum(result.values()) / len(result) == 0.0
 
 
 def test_missing_definition():
-    percent_match = compare_solution_assigment(
+    result = compare_solution_assignment(
         os.path.join(os.path.dirname(__file__), "solution_01", "solution.ino"),
         os.path.join(
             os.path.dirname(__file__), "assigments_01", "assigment_missing_def.ino"
         ),
     )
 
-    assert percent_match == -1.0
+    assert sum(result.values()) / len(result) == 50.0
 
 
 def test_compare_setups():
@@ -57,3 +59,10 @@ def test_compare_setups():
     )
 
     assert percent_match == 100.0
+
+
+def test_compare_folders():
+    result = batch_compare("solutions", "sample_student_submissions")
+
+    assert isinstance(result, pd.DataFrame)
+    assert len(result) > 0
